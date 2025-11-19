41a3593e85e65da84d40074cf80ac194bf5caa7d
alexandre michiels <alexoualexandre1@gmail.com>
2025-11-19 10:43:05 +0100

    fin word

--- src/StructureAppelEntrant.jsx
+++ src/StructureAppelEntrant.jsx
@@ -1,16 +1,15 @@
-
-
 function StructureAppelEntrant() {
   return (
-  <section className="section1">
-
-
-  <img src="structureAppelEntrant/1.jpg" className="img"/>
-<img src="structureAppelEntrant/2.jpg" className="img"/>
-<img src="structureAppelEntrant/3.jpg" className="img"/>
-<img src="structureAppelEntrant/4.jpg" className="img"/>
-<img src="structureAppelEntrant/5.jpg" className="img"/>
-</section>
-)}
+    <section className="section1">
+      <img src="structureAppelEntrant/1.jpg" className="img" />
+      <img src="structureAppelEntrant/2.jpg" className="img" />
+      <img src="structureAppelEntrant/3.jpg" className="img" />
+      <img src="structureAppelEntrant/4.jpg" className="img" />
+      <img src="structureAppelEntrant/5.jpg" className="img" />
+      <img src="structureAppelEntrant/6.jpg" className="img" />
+      <img src="structureAppelEntrant/7.jpg" className="img" />
+    </section>
+  );
+}
 
 export default StructureAppelEntrant;
--- src/Word.jsx
+++ src/Word.jsx
@@ -10,6 +10,14 @@ function Word() {
       <img src="word/3.jpg" className="img" />
       <img src="word/4.jpg" className="img" />
       <img src="word/5.jpg" className="img" />
+      <img src="word/6.jpg" className="img" />
+      <img src="word/7.jpg" className="img" />
+      <img src="word/8.jpg" className="img" />
+      <img src="word/9.jpg" className="img" />
+      <img src="word/10.jpg" className="img" />
+      <img src="word/11.jpg" className="img" />
+      <img src="word/12.jpg" className="img" />
+      <img src="word/13.jpg" className="img" />
     </section>
   );
 }
