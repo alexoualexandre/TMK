7fa7e2ad4bc73ca771777d64d159cb2991fb27ba
alexandre michiels <alexoualexandre1@gmail.com>
2025-12-30 22:56:00 +0100

    sms ok

--- src/Puissance.jsx
+++ src/Puissance.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Puissance() {
   const params = new URLSearchParams(window.location.search);
   const paramsId = params.get("id");
+  const buttonAdd = useRef();
 
   const [inviter, setInviter] = useState("");
+  const [insertNumber, setInsertNumber] = useState(false);
+  const [valueInput, setValueInput] = useState("");
   const { VITE_API_URL, VITE_API_SERVER_PORT, VITE_API_HTTP } = import.meta.env;
 
   useEffect(() => {
@@ -63,6 +66,29 @@ function Puissance() {
       });
   }, []);
 
+  const input = () => {
+    if (!/^(06|07)[0-9]{8}$/.test(valueInput)) {
+      setInsertNumber(true);
+    }
+  };
+
+  const focusOn = () => {
+    buttonAdd.current.style.opacity = 0;
+  };
+
+  const focusOff = () => {
+    buttonAdd.current.style.opacity = 1;
+    setInsertNumber(false);
+  };
+
+  const change = (e) => {
+    setValueInput(e.target.value);
+  };
+
+  if (/^(06|07)[0-9]{8}$/.test(valueInput)) {
+    buttonAdd.current.style.opacity = 1;
+  }
+
   //   useEffect(() => {
   //     function bdd() {
   //       const interval = setInterval(() => {
@@ -83,14 +109,35 @@ function Puissance() {
   return (
     <div className="div-puissance">
       <img src="P4.png" className="img-puissance4" />
-      <a
-        href={
-          inviter !== "" &&
-          `sms:0659834511?body=Envoyez lui ce lien et jouer ensemble à puissance 4 ! ${VITE_API_HTTP}://${VITE_API_URL}:5003/puissance4?id=${inviter}`
-        }
-      >
-        <button className="partage">inviter</button>
-      </a>
+
+      {!/^(06|07)[0-9]{8}$/.test(valueInput) ? (
+        <button className="partage" onClick={input} ref={buttonAdd}>
+          Jouer avec un ami
+        </button>
+      ) : (
+        <a
+          href={
+            inviter !== "" &&
+            `sms:${valueInput}?body=Envoyez lui ce lien et jouer ensemble à puissance 4 ! ${VITE_API_HTTP}://${VITE_API_URL}:5003/puissance4?id=${inviter}`
+          }
+        >
+          <button className="partage" ref={buttonAdd}>
+            Je l'invite !
+          </button>
+        </a>
+      )}
+
+      {insertNumber && !/^(06|07)[0-9]{8}$/.test(valueInput) && (
+        <input
+          type="text"
+          className="insert-number"
+          autoFocus
+          onFocus={focusOn}
+          onBlur={focusOff}
+          onChange={change}
+          placeholder="Son numéro de mobile ..."
+        />
+      )}
       {/* <div className="lesnoms"><h4>{users && users[0].nom1}</h4><h4>{users && users[0].nom2}</h4></div> */}
 
       <div className="parent">
